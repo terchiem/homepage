14cef76234981b31212a6fb0d41e1cc3b92ca23a
Terry Chiem <terrychiem@gmail.com>
2019-08-06 16:22:48 -0700

    add routing to component pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import About from './About';
+import Resume from './Resume';
+import Programming from './Programming';
+import OtherWork from './OtherWork';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello, world</p>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/resume/">Resume</Link>
+            </li>
+            <li>
+              <Link to="/programming/">Programming</Link>
+            </li>
+            <li>
+              <Link to="/otherwork/">Other Work</Link>
+            </li>
+            <li>
+              <Link to="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Route path="/about/" component={About} />
+        <Route path="/resume/" component={Resume} />
+        <Route path="/programming/" component={Programming} />
+        <Route path="/otherwork/" component={OtherWork} />
+        <Route path="/contact/" component={Contact} />
+
+      </Router>
     </div>
   );
 }
