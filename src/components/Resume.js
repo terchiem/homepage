b3601ee1054144b673d606a822a0b4580b7e69a8
Terry Chiem <terrychiem@gmail.com>
2019-08-08 12:09:58 -0700

    finish resume page content

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,66 @@
 import React from 'react';
 
+import { workList, educationList, skillList } from '../resume-list';
+
 function Resume() {
+  const workItems = workList.map(job => {
+    const tasks = job.description.map(task => <li>{task}</li>);
+
+    return (
+      <div className="resume-item">
+        <h4>{job.role} — <span>{job.company}</span> ({job.date})</h4>
+        <ul>
+          {tasks}
+        </ul>
+      </div>
+    )
+  })
+
+  const schoolItems = educationList.map(school => (
+    <div className="resume-item">
+      <h4><span>{school.name}</span> ({school.date})</h4>
+      <ul><li>{school.description}</li></ul>
+    </div>
+  ))
+
+  const skillItems = skillList.map(skill => <li>{skill}</li>)
+
   return (
-    <div className="page">
-      <p>I'm the Resume page!</p>
-      <a href="#">
-        Download Resume as PDF
+    <div className="page resume">
+      <a href="https://drive.google.com/open?id=135kb6Y3Opm67rOjUC8ctwxktV6QlBhU9">
+        <div className="download">
+          Download PDF
+        </div>
       </a>
+
+      <section>
+        <h3>Work Experience</h3>
+        {workItems}
+      </section>
+
+      <section>
+        <h3>Education</h3>
+        {schoolItems}
+      </section>
+
+      <section>
+        <div className="resume-other">
+          <div>
+            <h3>Skills</h3>
+            <ul>
+              {skillItems}
+            </ul>
+          </div>
+          <div>
+            <h3>Online Education</h3>
+            <h5>The Odin Project</h5>
+            <ul><li>Full Stack Ruby on Rails</li></ul>
+            <h5>Free Code Camp</h5>
+            <ul><li>Responsive Web Design, Javascript, Front End Libraries</li></ul>
+          </div>
+        </div>
+        
+      </section>
     </div>
   )
 }
---
+++ src/resume-list.js
@@ -0,0 +1,64 @@
+export const workList = [
+  {
+    company: 'Amazon Logistics',
+    role: 'Sortation Associate',
+    date: 'October 2016 - December 2017',
+    description: [
+      'Dispatcher: Assigned contract drivers their delivery routes, assisted contract drivers with technical and hardware issues regarding Amazon Flex app, processed returns on undeliverable packages',
+      'Problem Solver: Repacked damaged orders, resolved package issues, performed package cluster transfers'
+    ]
+  },
+  {
+    company: 'Telegenics Inc.',
+    role: 'Freelance Animator/Video Editor',
+    date: 'July 2015 - June 2017',
+    description: [
+      'Created video assets to support court case testimonials with an emphasis on precision ',
+      'Reconstructed accidents with 3D animation using data from police and witness reports',
+      'Composited 3D assets into video drone footage for land use cases'
+    ]
+  },
+  {
+    company: 'Zindagi Games',
+    role: 'Animator',
+    date: 'April 2015 – October 2015',
+    description: [
+      'Created 2D and 3D game animations for two mobile titles',
+      'Worked with a script to develop and lay out short story cutscenes',
+      'Maintained a schedule while working remotely under tight deadlines'
+    ]
+  },
+  {
+    company: 'Spark Unlimited',
+    role: 'Animation Intern',
+    date: 'June 2013 – November 2013',
+    description: [
+      'Created character gameplay and cinematic animations using Maya',
+      'Developed storyboard and layout for cut scenes and scene transitions'
+    ]
+  }
+]
+
+export const educationList = [
+  {
+    name: 'De Anza Community College',
+    date: 'January 2018 - June 2019',
+    description: 'Certificate of Achievement in Programming in C++ / Java / Python'
+  },
+  {
+    name: 'San Jose State University',
+    date: 'August 2005 - June 2012',
+    description: 'Bachelor of Fine Arts in Animation & Illustration'
+  }
+]
+
+export const skillList = [
+  'Javascript / ES6',
+  'HTML / CSS',
+  'Ruby / Rails',
+  'Python',
+  'React',
+  'Vue',
+  'Adobe CS',
+  
+]
