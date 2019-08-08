288d9f7fb4268fa85adc9dd306f07064b26c36a8
Terry Chiem <terrychiem@gmail.com>
2019-08-07 17:22:35 -0700

    complete contact component and layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 function Contact() {
   return (
-    <div className="page">
-      <p>I'm the Contact page!</p>
+    <div className="page contacts">
+      <h3 className="contacts-header">CONTACTS</h3>
+      <ul className="contacts-list">
+        <li>
+          <a href="mailto:terrychiem@gmail.com">
+            <FontAwesomeIcon icon={faEnvelopeSquare} size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/terrychiem">
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/terchiem/">
+            <FontAwesomeIcon icon={faGithubSquare} size="lg" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
