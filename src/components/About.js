8e2b62290e440a2fac705d1cf344a89fd5914c20
Terry Chiem <terrychiem@gmail.com>
2019-08-09 13:41:59 -0700

    complete content for About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 function About() {
   return (
     <div className="page">
-      <p>I'm the about page!</p>
+      <p>Welcome!</p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum quisque non tellus orci. Egestas congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius quam. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Urna neque viverra justo nec ultrices dui sapien eget mi. Pellentesque id nibh tortor id aliquet lectus. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Vitae semper quis lectus nulla at volutpat diam. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Mauris commodo quis imperdiet massa tincidunt nunc. Neque vitae tempus quam pellentesque nec nam aliquam. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Condimentum mattis pellentesque id nibh. At erat pellentesque adipiscing commodo elit. At consectetur lorem donec massa sapien faucibus et molestie ac. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Molestie at elementum eu facilisis sed odio morbi. Felis bibendum ut tristique et egestas quis ipsum. Cras ornare arcu dui vivamus.
+        My name is Terry and I am a self-taught web developer with a traditional art background. I originally went to school to study animation and illustration which transitioned into a career creating 3D animations for games and videos. Eventually I found myself stuck in a creative rut and stepped away to find a way to reinvent myself. 
       </p>
       <p>
-        Vel eros donec ac odio tempor orci. Lorem mollis aliquam ut porttitor leo. Aliquet lectus proin nibh nisl condimentum id venenatis a. Et pharetra pharetra massa massa ultricies mi quis hendrerit. Risus nullam eget felis eget nunc lobortis. Consequat nisl vel pretium lectus quam id leo in vitae. Ultricies leo integer malesuada nunc. At erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Enim neque volutpat ac tincidunt vitae semper. Tristique senectus et netus et. Tristique magna sit amet purus gravida quis blandit. Sed risus pretium quam vulputate dignissim suspendisse in. Amet venenatis urna cursus eget. Volutpat consequat mauris nunc congue nisi vitae. Mi proin sed libero enim sed faucibus turpis. Lorem dolor sed viverra ipsum. Habitant morbi tristique senectus et netus. A lacus vestibulum sed arcu non odio euismod.
+        I decided to take some programming classes at a community college and found out that I had a real knack for it. I realized that so many of the concepts I had learned in art also applied to programming. The importance of a strong design rather than its implementation was a principle that I had been so familiar with in my previous career. It has revitalized my passion for design and my goal is to find new ways to apply my old skills.
       </p>
     </div>
   )
