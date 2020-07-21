cdb508de7e19c2a0e19480a4427240a8bdcb881e
Terry Chiem <terrychiem@gmail.com>
2020-07-20 17:16:56 -0700

    Add country finder to projects

--- src/page-resources/project-list.js
+++ src/page-resources/project-list.js
@@ -1,5 +1,19 @@
 const projects = [
   {
+    name: "Where in the World?",
+    description: "A React Single Page Application that utilizes the REST Countries API to display a list of countries and some facts about them. Users can click on a country to learn more about it.",
+    details: [
+      "Features light/dark mode theme switching",
+      "Implemented performant live search and filtering of countries with Reselect",
+      "Use of Redux to store data from API and Redux Saga for asynchronous actions"
+    ],
+    tools: ["React", "Redux", "CSS", "Javascript", "Front End"],
+    image: "country",
+    git: "https://github.com/terchiem/rest-countries-api-react",
+    link: "https://rest-countries-api-react.vercel.app/",
+    codepen: ""
+  },
+  {
     name: "Jobly",
     description: "A Full Stack application for job searching. Users can log in to browse and search for companies or jobs. It utilizes a React client and an Express server connected to a Postgres DB.",
     details: [
@@ -48,42 +62,6 @@ const projects = [
     link: "",
     codepen: ""
   },
-  // {
-  //   name: "Weather App",
-  //   description: "A weather app that I used as a chance to learn Vue. This app uses the OpenWeather API and fetches a 5 day forecast for a given location. This project allowed me to learn about asynchronous functions and promises. I also created the weather condition icons using an online SVG editing tool.",
-  //   tools: ["Vue", "Javascript", "CSS", "HTML", "Front End"],
-  //   image: "weatherapp",
-  //   git: "https://github.com/terchiem/weather_app",
-  //   link: "https://terchiem.github.io/weather_app/",
-  //   codepen: ""
-  // },
-  // {
-  //   name: "Project/Task Tracker",
-  //   description: "A to-do list app I made while learning to use React. It allows the user to create projects and fill them with to-do list items. I had previously made version of this app using pure Javascript and used that as a jumping off point to learn React. It made me see the light of shared states and letting React handle the DOM.",
-  //   tools: ["React", "Javascript", "CSS", "HTML", "Front End"],
-  //   image: "todoapp",
-  //   git: "https://github.com/terchiem/todo_list_react",
-  //   link: "https://terchiem.github.io/todo_list_react/",
-  //   codepen: ""
-  // },
-  // {
-  //   name: "Tic-Tac-Toe",
-  //   description: "A game of Tic-Tac-Toe created with Javascript using modules and factories. The main goal of this project was to have as little global code as possible. This project definitely taught me the value of making code more modular.",
-  //   tools: ["Javascript", "CSS", "HTML", "Front End"],
-  //   image: "tictactoe",
-  //   git: "https://github.com/terchiem/js_tictactoe",
-  //   link: "https://terchiem.github.io/js_tictactoe/",
-  //   codepen: ""
-  // },
-  // {
-  //   name: "Pomodoro Clock",
-  //   description: "A browser timer for utilizing the Pomodoro Technique. One of my earliest projects that taught me how to work with intervals in Javascript and how to create a timer that toggles between two values.",
-  //   tools: ["Javascript", "CSS", "HTML", "Front End"],
-  //   image: "pomodoro",
-  //   git: "https://github.com/terchiem/pomodoro_clock",
-  //   link: "https://terchiem.github.io/pomodoro_clock/",
-  //   codepen: ""
-  // },
 ]
 
 export default projects;
