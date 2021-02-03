74abe5a7f1b0f010c8cc40d521cac887e6c17bd8
Terry Chiem <terrychiem@gmail.com>
2021-02-03 00:13:54 -0800

    Update about me

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
           My name is Terry and I am a Full Stack developer with a background in art and design. I previously had a career as a 3D animator and video producer on various projects. Throughout the years, I discovered that I really enjoyed solving technical problems and learning how things worked.
         </p>
         <p>
-          At the beginning of this year, I decided to take the step of changing my career. I enrolled in Rithm School and for 4 months, I worked hard to become a confident Full Stack developer. Now I am looking for opportunities to collaborate with others and build great things.
+          At the beginning of last year, I decided to take the step of changing my career. I enrolled in Rithm School and for 4 months, I worked hard to become a confident Full Stack developer. Now I am looking for opportunities to collaborate with others and build great things.
         </p>
       </div>
     </div>
